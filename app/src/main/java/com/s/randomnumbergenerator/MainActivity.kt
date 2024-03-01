868486a3e8a19377375480394dc1896b8b2da508
spasko123-Laptop <spas86512@gmail.com>
2024-03-01 17:28:36 +0200

    Test

--- app/src/main/java/com/s/randomnumbergenerator/MainActivity.kt
+++ app/src/main/java/com/s/randomnumbergenerator/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         yesOrNo = findViewById(R.id.yesOrNo)
+        //test ofr github
 
         yesOrNo.setOnClickListener {
             val intent = Intent(this, YesOrNoActivity::class.java)
